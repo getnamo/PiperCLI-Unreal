ab33c700cd53be0a41c5e6abea10cf3c6e3fb765
getnamo <getnamo@gmail.com>
2024-01-28 19:34:52 -0800

    scaffold component link

--- Source/PiperCLI/Private/PiperComponent.cpp
+++ Source/PiperCLI/Private/PiperComponent.cpp
@@ -1,9 +1,37 @@
 #include "PiperComponent.h"
+#include "Misc/Paths.h"
+
+
+UPiperComponent::UPiperComponent(const FObjectInitializer& init) : UActorComponent(init)
+{
+	PiperCLIParams.Url = FPaths::ConvertRelativePathToFull(FPaths::ProjectDir() + TEXT("ThirdParty/Piper/Win64/piper.exe"));
+}
+
+UPiperComponent::~UPiperComponent()
+{
+
+}
+
 
 void UPiperComponent::InitializeComponent()
 {
 	Super::InitializeComponent();
 	ProcessHandler = MakeShareable(new FSubProcessHandler);
+
+	ProcessHandler->OnProcessBegin = [this](const int32 ProcessId, bool bStartSucceded)
+	{
+		OnBeginProcessing.Broadcast(FString::Printf(TEXT("Sucess: %d"), bStartSucceded));
+	};
+
+	ProcessHandler->OnProcessOutput = [this](const int32 ProcessId, const FString & OutputString)
+	{
+		OnOutput.Broadcast(OutputString);
+	};
+
+	ProcessHandler->OnProcessEnd = [this](const int32 ProcessId, int32 ReturnCode)
+	{
+		OnEndProcessing.Broadcast(FString::Printf(TEXT("ReturnCode: %d"), ReturnCode));
+	};
 }
 
 void UPiperComponent::UninitializeComponent()
@@ -15,13 +43,16 @@ void UPiperComponent::UninitializeComponent()
 void UPiperComponent::BeginPlay()
 {
 	Super::BeginPlay();
+
+	if (bStartPiperOnBeginPlay)
+	{
+		ProcessHandler->StartProcess(PiperCLIParams);
+	}
 }
 
-UPiperComponent::UPiperComponent(const FObjectInitializer& init) : UActorComponent(init)
-{
-}
 
-UPiperComponent::~UPiperComponent()
+void UPiperComponent::EndPlay(const EEndPlayReason::Type EndPlayReason)
 {
-
+	ProcessHandler->StopProcess();
+	Super::EndPlay(EndPlayReason);
 }
--- Source/PiperCLI/Public/PiperComponent.h
+++ Source/PiperCLI/Public/PiperComponent.h
@@ -4,15 +4,36 @@
 #include "Components/ActorComponent.h"
 #include "PiperComponent.generated.h"
 
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FPiperBeginProcessSignature, const FString&, StartUpState);
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FPiperOutputSignature, const FString&, OutputMessage);
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FPiperEndProcessSignature, const FString&, EndState);
+
 UCLASS(BlueprintType, ClassGroup = "TTS", meta = (BlueprintSpawnableComponent))
 class PIPERCLI_API UPiperComponent : public UActorComponent
 {
 	GENERATED_UCLASS_BODY()
 public:
 
+	UPROPERTY(BlueprintAssignable, Category = "Piper Events")
+	FPiperOutputSignature OnOutput;
+
+	UPROPERTY(BlueprintAssignable, Category = "Piper Events")
+	FPiperBeginProcessSignature OnBeginProcessing;
+
+	UPROPERTY(BlueprintAssignable, Category = "Piper Events")
+	FPiperEndProcessSignature OnEndProcessing;
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "Piper Params")
+	FProcessParams PiperCLIParams;
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "Piper Params")
+	bool bStartPiperOnBeginPlay = true;
+
+
 	virtual void InitializeComponent() override;
 	virtual void UninitializeComponent() override;
 	virtual void BeginPlay() override;
+	virtual void EndPlay(const EEndPlayReason::Type EndPlayReason) override;
 
 	~UPiperComponent();
 
